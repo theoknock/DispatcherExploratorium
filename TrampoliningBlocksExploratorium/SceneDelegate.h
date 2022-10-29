711483697d095166481837a331fbaa5fc281b96b
James Alan Bush <jamesalanbush1972@gmail.com>
2022-10-29 16:44:02 -0400

    Initial Commit

---
+++ TrampoliningBlocksExploratorium/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  TrampoliningBlocksExploratorium
+//
+//  Created by Xcode Developer on 10/29/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ TrampoliningBlocksExploratorium/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  TrampoliningBlocksExploratorium
+//
+//  Created by Xcode Developer on 10/29/22.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
